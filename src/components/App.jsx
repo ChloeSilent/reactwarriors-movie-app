281ac888e5953af453c598993627b57766c2dcfb
OlgaShitova <os@3d.ru>
2019-12-03 19:56:15 +0300

    13z минута

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,60 @@ import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-4">
-            <div className="card" style={{ width: "100%" }}>
-              <div className="card-body">
-                <h3>Фильтры:</h3>
-                <Filters />
-              </div>
+
+    constructor() {
+        super();
+        this.state = {
+            filters: {
+                sort_by: "vote_average.desc"
+            },
+            page: 1
+        }
+    }
+
+    onChangeFilters = (event) => {
+        const {name, value} = event.target;
+        const newFilters = {
+            ...this.state.filters,
+            [name]: [value]
+        };
+
+        this.setState(prevState => ({
+                filters: newFilters
+            })
+        )
+    };
+
+    onChangePage = page => {
+        this.setState({
+          page
+        })
+    };
+
+    render() {
+        const {filters} = this.state;
+
+        return (
+            <div className="container">
+                <div className="row mt-4">
+                    <div className="col-4">
+                        <div className="card" style={{width: "100%"}}>
+                            <div className="card-body">
+                                <h3>Фильтры:</h3>
+                                <Filters filters={filters}
+                                         onChangeFilters={this.onChangeFilters}
+                                         page={this.state.page}
+                                         onChangePage={this.onChangePage}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-8">
+                        <MoviesList filters={filters}
+                                    page={this.state.page}
+                                    onChangePage={this.onChangePage}/>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="col-8">
-            <MoviesList />
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -1,41 +1,62 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import MovieItem from "./MovieItem";
-import { API_URL, API_KEY_3 } from "../../api/api";
+import {API_URL, API_KEY_3} from "../../api/api";
 
 export default class MovieList extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      movies: []
+        this.state = {
+            movies: []
+        };
+    }
+
+    getMovies = (filters, page) => {
+        const {sort_by} = filters
+        const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}&page=${page}`;
+        fetch(link)
+            .then(response => {
+
+                return response.json();
+            })
+            .then(data => {
+
+                this.setState({
+                    movies: data.results
+                });
+            });
     };
-  }
 
-  componentDidMount() {
-    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU`;
-    fetch(link)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        this.setState({
-          movies: data.results
-        });
-      });
-  }
 
-  render() {
-    const { movies } = this.state;
-    return (
-      <div className="row">
-        {movies.map(movie => {
-          return (
-            <div key={movie.id} className="col-6 mb-4">
-              <MovieItem item={movie} />
+    componentDidMount() {
+        this.getMovies(this.props.filters);
+    };
+
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+        if (this.props.filters.sort_by !== prevProps.filters.sort_by) {
+            this.getMovies(this.props.filters, 1);
+            this.props.onChangePage(1);
+        }
+      if (this.props.page !== prevProps.page) {
+        this.getMovies(this.props.filters, this.props.page);
+        console.log("componentDidUpdate", prevProps.page, this.props.page)
+      }
+    }
+
+    render() {
+        const {movies} = this.state;
+        return (
+            <div className="row">
+                {movies.map(movie => {
+                    return (
+                        <div key={movie.id} className="col-6 mb-4">
+                            <MovieItem item={movie}/>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    );
-  }
+        );
+    }
 }
