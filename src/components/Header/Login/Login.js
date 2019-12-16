7832744fe4b17e4c49d8466d6bb88b5c6708e458
Olga <hloesilent@gmail.com>
2019-12-17 00:11:21 +0300

    цепочка фетчей

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -11,11 +11,36 @@ export default class Login extends React.Component {
         fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
+                console.log(data);
+                fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
+                        method: "POST",
+                        mode: "cors",
+                        "headers": {
+                            "Content-type": "application/json"
+                        },
+                        body: JSON.stringify(
+                            {
+                                username: "ChloeSilent",
+                                password: "ReactWarriors",
+                                request_token: data.request_token
+                            })
+                    }
+                ).then(response => response.json()).then(data => {
+                    fetch(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
+                        method: "POST",
+                        mode: "cors",
+                        "headers": {
+                            "Content-type": "application/json"
+                        },
+                        body: JSON.stringify(
+                            {
+                                request_token: data.request_token
+                            })
+                    }).then(response => response.json())
+                        .then(data => console.log("session", data))
+                })
             })
     };
-
-
 
 
     render() {
